cbd8219785fe66c6eca3e8619f8a774007b7fe12
Adam Hellberg <sharparam@sharparam.com>
2017-08-17 22:39:11 +0200

    Add a pretty printer for expression trees

---
+++ src/main/kotlin/com/sharparam/klox/AstPrinter.kt
@@ -0,0 +1,27 @@
+package com.sharparam.klox
+
+class AstPrinter: Expression.Visitor<String> {
+    fun print(expr: Expression): String = expr.accept(this)
+
+    override fun visit(expr: Expression.Binary) = parenthesize(expr.operator.lexeme, expr.left, expr.right)
+
+    override fun visit(expr: Expression.Grouping) = parenthesize("group", expr.expression)
+
+    override fun visit(expr: Expression.Literal) = expr.value?.toString() ?: "nil"
+
+    override fun visit(expr: Expression.Unary) = parenthesize(expr.operator.lexeme, expr.right)
+
+    private fun parenthesize(name: String, vararg exprs: Expression): String {
+        val builder = StringBuilder()
+
+        builder.append("(").append(name)
+
+        exprs.forEach {
+            builder.append(" ").append(it.accept(this))
+        }
+
+        builder.append(")")
+
+        return builder.toString()
+    }
+}
