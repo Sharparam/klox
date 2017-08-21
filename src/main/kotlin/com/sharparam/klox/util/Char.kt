9042ba16c42b91d0f1ce8d1e8651e68c4927e99f
Adam Hellberg <sharparam@sharparam.com>
2017-08-21 16:39:26 +0200

    Clean up the utility code

--- src/main/kotlin/com/sharparam/klox/util/Char.kt
+++ src/main/kotlin/com/sharparam/klox/util/Char.kt
@@ -1,5 +1,5 @@
 package com.sharparam.klox.util
 
-val Char.isAlpha get() = this in 'a'..'z' || this in 'A'..'Z' || this == '_'
+internal val Char.isAlpha get() = this in 'a'..'z' || this in 'A'..'Z' || this == '_'
 
-val Char.isAlphaNumeric get() = isAlpha || this in '0'..'9'
+internal val Char.isAlphaNumeric get() = isAlpha || this in '0'..'9'
--- src/main/kotlin/com/sharparam/klox/util/Logging.kt
+++ src/main/kotlin/com/sharparam/klox/util/Logging.kt
@@ -4,9 +4,9 @@ import org.slf4j.LoggerFactory
 import kotlin.reflect.KClass
 import kotlin.reflect.full.companionObject
 
-fun <T: Any> T.logger() = lazy { LoggerFactory.getLogger(unwrapCompanionClass(this.javaClass)) }
+internal fun <T: Any> T.logger() = lazy { LoggerFactory.getLogger(unwrapCompanionClass(this.javaClass)) }
 
-fun logger(name: String) = lazy { LoggerFactory.getLogger(name) }
+internal fun logger(name: String) = lazy { LoggerFactory.getLogger(name) }
 
 private fun <T: Any> unwrapCompanionClass(ofClass: Class<T>): Class<*> {
     return if (ofClass.enclosingClass != null && ofClass.enclosingClass.kotlin.companionObject?.java == ofClass) {
@@ -15,5 +15,3 @@ private fun <T: Any> unwrapCompanionClass(ofClass: Class<T>): Class<*> {
         ofClass
     }
 }
-
-private fun <T: Any> unwrapCompanionClass(ofClass: KClass<T>): KClass<*> = unwrapCompanionClass(ofClass.java).kotlin
